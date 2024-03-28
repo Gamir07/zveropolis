0dc17cc3916bee4aa4e6b1938a6e0d5e37845122
Надежда <nadin94403@gmail.com>
2024-03-28 21:49:53 +0300

    подгрузила телеграмм-бота, создала базу данных (роль входа+ пароль), прописала в properties. Создала liquibase, файл скрипта.

--- src/main/java/ru/skypro/zveropolis/configuration/TelegramBotConfiguration.java
+++ src/main/java/ru/skypro/zveropolis/configuration/TelegramBotConfiguration.java
@@ -1,22 +1,19 @@
 package ru.skypro.zveropolis.configuration;
-
-import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.model.DeleteMyCommands;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@Data
+@PropertySource("application.properties")
 public class TelegramBotConfiguration {
 
     @Value("${telegram.bot.token}")
     private String token;
 
-    @Bean
-    public TelegramBot telegramBot() {
-        TelegramBot bot = new TelegramBot(token);
-        bot.execute(new DeleteMyCommands());
-        return bot;
-    }
+    @Value("${telegram.bot.name}")
+    private String name;
 
 }
+
