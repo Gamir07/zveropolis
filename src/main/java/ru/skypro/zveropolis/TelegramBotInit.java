d98a6eb2e6e5554644406cf5de51d6e90319c885
MSI <kulakov056@mail.ru>
2024-04-02 21:51:07 +0500

    add repository naya

---
+++ src/main/java/ru/skypro/zveropolis/TelegramBotInit.java
@@ -0,0 +1,44 @@
+package ru.skypro.zveropolis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.skypro.zveropolis.configuration.TelegramBotConfiguration;
+
+@Component
+@RequiredArgsConstructor
+public class TelegramBotInit extends TelegramLongPollingBot {
+
+    private final TelegramBotConfiguration telegramBotConfiguration;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Long chatId = update.getMessage().getChatId();
+        String text = update.getMessage().getText();
+        String message = "привет";
+        if (update.hasMessage()&&update.getMessage().hasText()) {
+            SendMessage sendMessage= new SendMessage();
+            sendMessage.setChatId(chatId);
+            sendMessage.setText(message);
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+    @Override
+    public String getBotUsername() {
+        return telegramBotConfiguration.getName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return telegramBotConfiguration.getToken();
+    }
+}
