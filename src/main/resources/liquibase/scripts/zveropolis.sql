46faf67069ea04d7202288f9599437d061c9955b
Надежда <nadin94403@gmail.com>
2024-04-03 00:16:50 +0300

    правки с аннотациями и именем переменной

--- src/main/resources/liquibase/scripts/zveropolis.sql
+++ src/main/resources/liquibase/scripts/zveropolis.sql
@@ -4,12 +4,12 @@ create table cat (
 id bigint primary key,
 name text,
 age integer,
-limitedOpportunities boolean
+isHandicapped boolean
 )
 -- changeset nbelousova:2
 create table dog (
 id bigint primary key,
 name text,
 age integer,
-limitedOpportunities boolean
+isHandicapped boolean
 )
