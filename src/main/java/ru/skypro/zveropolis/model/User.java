4655eca9d417e51c7150c126c1ce77f1c1c1b1a5
Admin <samedi450511@gmail.com>
2024-04-07 18:02:42 +0600

    added relocation

--- src/main/java/ru/skypro/zveropolis/model/User.java
+++ src/main/java/ru/skypro/zveropolis/model/User.java
@@ -1,25 +1,22 @@
-//package ru.skypro.zveropolis.model;
-//
-//import lombok.Data;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//import javax.persistence.*;
-//
-//@Entity
-//@Data
-//@Table(schema = "user")
-//public class User {
-//
-//    @Id
-//    private long chatId;
-//    private String name;
-//    private String phoneNumber;
-//    private boolean isVolunteer;
-//
-////    @OneToOne
-////    @JoinColumn(name = "pet_id")
-////    private Pet pet;
-//
-//}
+package ru.skypro.zveropolis.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(schema = "user")
+public class User {
+
+    @Id
+    private long chatId;
+    private String name;
+    private String phoneNumber;
+    private boolean isVolunteer;
+
+    @OneToOne
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+
+}
--- src/main/java/ru/skypro/zveropolis/relocation/Relocation.java
+++ src/main/java/ru/skypro/zveropolis/relocation/Relocation.java
@@ -1,14 +1,53 @@
 package ru.skypro.zveropolis.relocation;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.skypro.zveropolis.TelegramBotSendMessage;
+import ru.skypro.zveropolis.model.User;
+import ru.skypro.zveropolis.repository.UserRepository;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 public class Relocation {
+    private final UserRepository userRepository;
+    private final StartMenu startMenu;
+    private Map<Long, StateBot> states = new HashMap<>();
 
-    public void doCrossroads(Update update){
+    public void doCrossroads(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            Long chatId = update.getMessage().getChatId();
+            checkUser(chatId);
+        } else if (update.hasCallbackQuery()) {
+            Long chatId = update.getCallbackQuery().getMessage().getChatId();
+            checkUser(chatId);
+        }
+    }
+
+    private boolean checkUser(Long chatId) {
+        if (states.containsKey(chatId)) {
+            return true;
+        }
+        User user = new User();
+        user.setChatId(chatId);
+        userRepository.save(user);
+        states.put(chatId, StateBot.START_MENU);
+        return false;
+    }
+
+    private State getStateFromText(Update update) {
+        Long chatId = update.getMessage().getChatId();
+        StateBot stateBot = states.get(chatId);
+        switch (stateBot) {
+            case START_MENU -> {
+                return startMenu;
+            }
+        }
+        throw new RuntimeException();
+        //todo исправить
     }
 }
