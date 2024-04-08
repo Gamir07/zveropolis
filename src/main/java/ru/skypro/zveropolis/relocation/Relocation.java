debc4aa3a1e480ae85cd3043d9f189003b59d068
Admin <samedi450511@gmail.com>
2024-04-08 21:27:33 +0600

    added relocation.2

--- src/main/java/ru/skypro/zveropolis/relocation/Relocation.java
+++ src/main/java/ru/skypro/zveropolis/relocation/Relocation.java
@@ -1,10 +1,8 @@
 package ru.skypro.zveropolis.relocation;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import ru.skypro.zveropolis.TelegramBotSendMessage;
 import ru.skypro.zveropolis.model.User;
 import ru.skypro.zveropolis.repository.UserRepository;
 
@@ -16,32 +14,35 @@ import java.util.Map;
 public class Relocation {
     private final UserRepository userRepository;
     private final StartMenu startMenu;
-    private Map<Long, StateBot> states = new HashMap<>();
+    private Map<Long, StateBot> stateMap = new HashMap<>();
 
     public void doCrossroads(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             Long chatId = update.getMessage().getChatId();
             checkUser(chatId);
+            State state = getState(chatId);
+            state.execute(update);
         } else if (update.hasCallbackQuery()) {
             Long chatId = update.getCallbackQuery().getMessage().getChatId();
             checkUser(chatId);
+            State state = getState(chatId);
+            state.execute(update);
         }
     }
 
     private boolean checkUser(Long chatId) {
-        if (states.containsKey(chatId)) {
+        if (stateMap.containsKey(chatId)) {
             return true;
         }
         User user = new User();
         user.setChatId(chatId);
         userRepository.save(user);
-        states.put(chatId, StateBot.START_MENU);
+        stateMap.put(chatId, StateBot.START_MENU);
         return false;
     }
 
-    private State getStateFromText(Update update) {
-        Long chatId = update.getMessage().getChatId();
-        StateBot stateBot = states.get(chatId);
+    private State getState(Long chatId) {
+        StateBot stateBot = stateMap.get(chatId);
         switch (stateBot) {
             case START_MENU -> {
                 return startMenu;
