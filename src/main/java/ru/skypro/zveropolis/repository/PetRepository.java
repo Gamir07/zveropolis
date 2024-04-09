66ba695e0b1220bf150d14e9523233f771c4a65f
Admin <samedi450511@gmail.com>
2024-04-09 20:04:46 +0600

    doesn't see the class Pet

--- src/main/java/ru/skypro/zveropolis/repository/PetRepository.java
+++ src/main/java/ru/skypro/zveropolis/repository/PetRepository.java
@@ -3,7 +3,8 @@ package ru.skypro.zveropolis.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.skypro.zveropolis.model.Pet;
+
 @Repository
-public interface PetRepository extends JpaRepository<Pet,Long>  {
+public interface PetRepository extends JpaRepository<Pet, Long> {
     Pet findById(long id);
 }
